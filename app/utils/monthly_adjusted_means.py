cdeb55648e65ce30758769bf4c5f697e947b81ef
Paul Davis <pjdavis@gmx.com>
2019-10-17 00:30:06 +0100

    Tidy up month adj means

--- app/utils/monthly_adjusted_means.py
+++ app/utils/monthly_adjusted_means.py
@@ -1,8 +1,6 @@
 import logging
-from collections import Counter, OrderedDict
 
-import numpy as np
-import pandas as pd
+from pandas import date_range, Timedelta, Timestamp
 
 log = logging.getLogger(__name__)
 
@@ -17,12 +15,13 @@ def get_interpolated_values(day_diffs, data_points, name):
             break
         vpdpd = abs(lat_val - data_points[ind + 1][name]) / n
         for i in range(n):
-            if negative:
-                values.append(lat_val + vpdpd * i)
-            else:
-                values.append(lat_val - vpdpd * i)
+            values.append(
+                (lat_val + vpdpd * i) if negative else (lat_val - vpdpd * i)
+            )
         if ind + 1 == len(day_diffs):
-            values.append(values[-1] + vpdpd if negative else - vpdpd)
+            values.append(
+                (values[-1] + vpdpd) if negative else (values[-1] - vpdpd)
+            )
     return values
 
 
@@ -37,101 +36,55 @@ def get_monthly_means(data_points, extra=None):
         extra = {}
     if len(data_points) < 2:
         return []
-
     has_val2 = len([dp['value2'] for dp in data_points
                     if dp.get('value2')]) == len(data_points)
 
-    day_diffs, a,b,c = get_diffs(
-        data_points, has_val2=False
-    )
-
-    latest_date = pd.Timestamp(data_points[0]['date'])
+    day_diffs = get_day_diffs(data_points)
+    latest_date = Timestamp(data_points[0]['date'])
     latest_date_str = latest_date.strftime("%Y-%b")
-    max_days = pd.Timedelta(days=min(sum(day_diffs), 375))
+    max_days = Timedelta(days=min(sum(day_diffs), 375))
     earliest_date = latest_date - max_days
-    months_dt = pd.date_range(earliest_date,
-                              latest_date, freq='M').tolist()[::-1]
-    days_range = pd.date_range(earliest_date,
-                               latest_date, freq='D').tolist()[::-1]
-
-
-
-    # make a dataframe axis is date_range, col1 if value, col2 is value2,
-    # fill in actual values
-    # interpolate na values
-    # split/subset by month (cast/make new column of dates -> %Y-%m and split on
-    # average values within these
-
-
-    print(day_diffs)
-    print(sum(day_diffs))
-    print(len(days_range))
-    print(earliest_date)
-    print(latest_date)
-
-
+    months_dt = date_range(earliest_date,
+                           latest_date, freq='M').tolist()[::-1]
+    days_range = date_range(earliest_date,
+                            latest_date, freq='D').tolist()[::-1]
 
     values = get_interpolated_values(day_diffs, data_points, 'value')
-    df_data = {'value': values}
+    month_keys = [latest_date_str] + [a.strftime("%Y-%b") for a in months_dt]
+    months = get_monthly_values(values, days_range, month_keys)
+    months2 = []
     if has_val2:
-        df_data['value2'] = get_interpolated_values(day_diffs, data_points,
-                                                    'value2')
-    print(len(values))
-    print(len(df_data))
-    print(len(data_points))
-    foo = {k: [] for k in [latest_date_str] + [a.strftime("%Y-%b")
-                                               for a in months_dt]}
-    months = [a.strftime("%Y-%b") for a in months_dt]
+        values2 = get_interpolated_values(day_diffs, data_points, 'value2')
+        months2 = get_monthly_values(values2, days_range, month_keys)
+
+    monthly_means = []
+    for k, v in months.items():
+        data = {'month': k, 'value': v, **extra}
+        if has_val2 and months.keys() == months2.keys():
+            data['value2'] = months2[k]
+        monthly_means.append(data)
+
+    return monthly_means
+
+
+def get_monthly_values(values, days_range, month_keys):
+    months = {k: [] for k in month_keys}
     for ind, value in enumerate(values):
-        month_key = days_range[ind].strftime("%Y-%b")
-        foo[month_key].append(value)
-
-    print(foo)
-
-    print(len(values))
-    print(len(df_data))
-    print(len(data_points))
-
-    print(values)
-
-    df = pd.DataFrame(df_data)
-    print(df.shape)
-    print(df)
-
-    months = [a.strftime("%Y-%b") for a in months_dt]
-    monthly_changes = []
-    for month in months:
-        data = {'month': month,
-                # 'value': value,
-                'value': 5,
-                **extra}
-
-        monthly_changes.append(data)
-
-    return monthly_changes
+        try:
+            months[days_range[ind].strftime("%Y-%b")].append(value)
+        except IndexError:
+            break
+    for k, v in months.items():
+        months[k] = round(sum(v) / len(v), 1)
+    return months
 
 
-# def get_pccpm(mpdd, vdpm, mpdd_ind=0):
-#     while
-
-
-
-
-
-def get_diffs(data_points, has_val2):
-    day_diffs, month_diffs, val_pc_diffs, val2_pc_diffs = [[] for _ in range(4)]
+def get_day_diffs(data_points):
+    day_diffs = []
     for i, dp in enumerate(data_points):
         if i > 0:
-            dp_min1 = data_points[i - 1]
             day_diffs.append(
-                abs((pd.Timestamp(dp['date']) - pd.Timestamp(
+                abs((Timestamp(dp['date']) - Timestamp(
                     data_points[i - 1]['date'])).days)
             )
-            val_pc_diffs.append(
-                -((dp['value'] - dp_min1['value']) / dp['value']) * 100
-            )
-            if has_val2:
-                val2_pc_diffs.append(
-                    -((dp['value2'] - dp_min1['value2']) / dp['value2']) * 100
-                )
-    return day_diffs, month_diffs, val_pc_diffs, val2_pc_diffs
+    return day_diffs
