7848c9aeeb2645d48bbdae9c3973b65f2f1cfa31
Paul Davis <pjdavis@gmx.com>
2019-11-25 23:50:47 +0000

    Update report generate

--- app/tasks/generate_report.py
+++ app/tasks/generate_report.py
@@ -1,12 +1,12 @@
 import io
+from operator import itemgetter
 
 import seaborn as sns
 from celery.utils.log import get_task_logger
-from django.template import Context
 from django.template.loader import get_template
 from xhtml2pdf import pisa
 
-from app.models import Profile
+from app.models import Parameter, Profile, ProfileParamUnitOption
 from app.utils import get_monthly_means, get_rolling_mean
 from btk2.celery import celery_app
 
@@ -14,14 +14,15 @@ log = get_task_logger(__name__)
 
 
 # @celery_app.task
-def profile_report_pdf(profile_id):
+def profile_report_pdf(profile_id, date='20th Feb 2018'):
     try:
         profile = Profile.objects.get(id=profile_id)
     except Profile.DoesNotExist as e:
         log.error(e)
         return
     report_data = {
-        'items_list': get_items_list(profile.all_datapoints())
+        'items_list': get_items_list(profile.all_datapoints(), profile),
+        'date': date
     }
     pdf = render_to_pdf('profile_report.html', report_data)
     with open('foobar.pdf', 'wb') as file:
@@ -40,20 +41,39 @@ def render_to_pdf(template_src, context_dct):
         return result.getvalue()
 
 
-def get_items_list(all_dps):
-    dp_dct = {
-        'datapoints': [{**{field: getattr(obj, field) for field in
-                           ['date', 'value', 'value2']},
-                        **{'parameter': obj.parameter.name,
-                           'num_values': obj.parameter.num_values}}
-                       for obj in all_dps]
-    }
-    param_names = sorted(set([obj.parameter.name for obj in all_dps]))
+def get_items_list(all_dps, profile):
+    dp_lst = [{**{k: getattr(obj, k) for k in ['date', 'value', 'value2']},
+               'parameter': obj.parameter.name} for obj in all_dps]
+    param_names = sorted(
+        set([(obj.parameter.name,
+              obj.parameter.num_values == 2,
+              obj.parameter.value2_short_label_1,
+              obj.parameter.value2_short_label_2,
+              obj.parameter.custom_symbol)
+             for obj in all_dps]),
+        key=itemgetter(0)
+    )
+    unit_symbols = []
+    for tpl in param_names:
+        symbol = tpl[4]
+        if not symbol:
+            try:
+                symbol = ProfileParamUnitOption.objects.get(
+                    parameter__name=tpl[0], profile=profile
+                ).unit_option.symbol
+            except ProfileParamUnitOption.DoesNotExist as e:
+                log.error(e)
+                symbol = 'n/a'
+        unit_symbols.append(symbol)
     items_list = [{
         'name': name,
+        'unit_symbol': ind,
+        'val2_label_1': lab1,
+        'val2_label_2': lab2,
+        'has_val2': has_val2,
         'records': [{k: dct[k] for k in ['date', 'value', 'value2']}
-                    for dct in dp_dct['datapoints'] if dct['parameter'] == name]
-    } for name in param_names]
+                    for dct in dp_lst if dct['parameter'] == name]
+    } for ind, (name, has_val2, lab1, lab2, _) in enumerate(param_names)]
     for ind, item in enumerate(items_list):
         dps, extra = [{k: dct[k] for k in ['date', 'value', 'value2']}
                       for dct in item['records']], {'param_name': item['name']}
@@ -61,4 +81,5 @@ def get_items_list(all_dps):
             'rolling_means': get_rolling_mean(dps, extra=extra),
             'monthly_changes': get_monthly_means(dps, extra=extra)
         })
+    print(items_list)
     return items_list
