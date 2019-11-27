871768520b8014927d689968fb9d3f29d0aad1a8
Paul Davis <pjdavis@gmx.com>
2019-11-27 00:18:33 +0000

    Update report generate

--- app/tasks/generate_report.py
+++ app/tasks/generate_report.py
@@ -1,12 +1,16 @@
+import base64
 import io
 from operator import itemgetter
 
+import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 from celery.utils.log import get_task_logger
 from django.template.loader import get_template
+from matplotlib import style
 from xhtml2pdf import pisa
 
-from app.models import Parameter, Profile, ProfileParamUnitOption
+from app.models import Profile, ProfileParamUnitOption
 from app.utils import get_monthly_means, get_rolling_mean
 from btk2.celery import celery_app
 
@@ -33,17 +37,13 @@ def render_to_pdf(template_src, context_dct):
     template = get_template(template_src)
     html = template.render(context_dct)
     result = io.BytesIO()
-    pdf = pisa.pisaDocument(io.BytesIO(html.encode("ISO-8859-1")), result)
+    pdf = pisa.pisaDocument(io.BytesIO(html.encode("UTF-8")), result)
     if not pdf.err:
-        log.info('Result getvalue')
-        log.info(result.getvalue())
-        log.info(type(result.getvalue()))
         return result.getvalue()
+    log.error(pdf.error)
 
 
 def get_items_list(all_dps, profile):
-    dp_lst = [{**{k: getattr(obj, k) for k in ['date', 'value', 'value2']},
-               'parameter': obj.parameter.name} for obj in all_dps]
     param_names = sorted(
         set([(obj.parameter.name,
               obj.parameter.num_values == 2,
@@ -67,19 +67,92 @@ def get_items_list(all_dps, profile):
         unit_symbols.append(symbol)
     items_list = [{
         'name': name,
-        'unit_symbol': ind,
+        'unit_symbol': unit_symbols[ind],
         'val2_label_1': lab1,
         'val2_label_2': lab2,
         'has_val2': has_val2,
-        'records': [{k: dct[k] for k in ['date', 'value', 'value2']}
-                    for dct in dp_lst if dct['parameter'] == name]
+        'records': [{k: getattr(obj, k) for k in ['date', 'value', 'value2']}
+                    for obj in all_dps if obj.parameter.name == name]
     } for ind, (name, has_val2, lab1, lab2, _) in enumerate(param_names)]
     for ind, item in enumerate(items_list):
         dps, extra = [{k: dct[k] for k in ['date', 'value', 'value2']}
                       for dct in item['records']], {'param_name': item['name']}
         items_list[ind].update({
             'rolling_means': get_rolling_mean(dps, extra=extra),
-            'monthly_changes': get_monthly_means(dps, extra=extra)
+            'monthly_means': get_monthly_means(dps, extra=extra)
         })
-    print(items_list)
+
+        prm_fiels = ['unit_symbol', 'has_val2', 'val2_label_1', 'val2_label_2']
+        recs_plot = None
+        if len(item['records']) > 3:
+            plot_bio = make_chart_from_data(
+                item['records'], key='', **{k: item[k] for k in prm_fiels}
+            )
+            recs_plot = base64.b64encode(plot_bio.getvalue()).decode('ascii')
+        rms_plot = None
+        rms = get_rolling_mean(dps, extra=extra)
+        if len(rms) > 3:
+            plot_bio = make_chart_from_data(
+                rms, key='rolling_means', **{k: item[k] for k in prm_fiels}
+            )
+            rms_plot = base64.b64encode(plot_bio.getvalue()).decode('ascii')
+        mms_plot = None
+        mms = get_monthly_means(dps, extra=extra)
+        if len(mms) > 3:
+            plot_bio = make_chart_from_data(
+                mms, key='monthly_means', **{k: item[k] for k in prm_fiels}
+            )
+            mms_plot = base64.b64encode(plot_bio.getvalue()).decode('ascii')
+
+        items_list[ind].update({
+            'records_plot': recs_plot,
+            'rolling_means_plot': rms_plot,
+            'monthly_means_plot': mms_plot
+        })
     return items_list
+
+
+def make_chart_from_data(means_data, key='', **kwargs):
+    title_map = {'rolling_means': 'Moving averages',
+                 'monthly_means': 'Monthly averages'}
+    fig = plt.figure(figsize=(7, 3))
+    plt.title(title_map.get(key, ''), fontsize=14, fontname='Arial', loc='left',
+              pad=3)
+    plt.ylabel(kwargs.get('unit_symbol', ''), fontname='Arial', fontsize=12)
+    dtf = '%Y-%m-%d'
+    if key != 'rolling_means':
+        dtf = '%Y-%b'
+    df = pd.DataFrame(
+        {pd.datetime.strptime(d['date'], dtf): [d.get(f'value{i}', '')
+                                                for i in ['', 2]]
+         for d in means_data[::-1]},
+        index=[kwargs.get(f'val2_label_{a}', f'value {a}') for a in [1, 2]]
+    )
+    ss = df.iloc[0, :]
+    ss.plot(color="coral", linewidth=2, linestyle="-")
+    # ss.plot(figsize=(10, 6), color="coral", linewidth=2, linestyle="-")
+    if kwargs.get('has_val2'):
+        ss2 = df.iloc[1, :]
+        ss2.plot(color="cornflowerblue", linewidth=2, linestyle="-")
+        plt.legend(loc='upper right', fontsize='small')
+    bio = io.BytesIO()
+    # save figure to bytesio
+    fig.savefig(bio, format="png", bbox_inches='tight')
+    # plt.show()
+    plt.clf()
+    plt.close(fig)
+    return bio
+
+
+# rms = [
+#     {'date': '2019-09-09', 'value': 29.0, 'param_name': 'Blood cholesterol', 'value2': 41.0},
+#     {'date': '2019-07-09', 'value': 28.3, 'param_name': 'Blood cholesterol', 'value2': 41.3},
+#     {'date': '2019-04-09', 'value': 28.8, 'param_name': 'Blood cholesterol', 'value2': 40.0},
+#     {'date': '2019-01-24', 'value': 28.2, 'param_name': 'Blood cholesterol', 'value2': 40.0},
+#     {'date': '2018-10-10', 'value': 28.5, 'param_name': 'Blood cholesterol', 'value2': 40.0},
+#     {'date': '2018-09-19', 'value': 28.5, 'param_name': 'Blood cholesterol', 'value2': 39.8},
+#     {'date': '2018-05-10', 'value': 27.2, 'param_name': 'Blood cholesterol', 'value2': 39.5},
+#     {'date': '2017-10-10', 'value': 27.2, 'param_name': 'Blood cholesterol', 'value2': 38.8},
+#     {'date': '2016-04-11', 'value': 27.2, 'param_name': 'Blood cholesterol', 'value2': 37.0},
+#     {'date': '2015-10-11', 'value': 30.5, 'param_name': 'Blood cholesterol', 'value2': 39.2}
+# ]
